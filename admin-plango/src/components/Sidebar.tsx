bcb63d29dd33ef66e8b411350418c7e2b13fd4ba
thaolethao <lethithao149204@gmail.com>
2026-04-30 22:53:25 +0700

    Sửa admin

--- admin-plango/src/components/Sidebar.tsx
+++ admin-plango/src/components/Sidebar.tsx
@@ -43,11 +43,11 @@ const Sidebar: React.FC = () => {
     { icon: <Users size={20} />, label: 'Người dùng', to: '/users' },
     { icon: <MapPin size={20} />, label: 'Điểm đến', to: '/destinations' },
     { icon: <Map size={20} />, label: 'Địa điểm (POI)', to: '/poi' },
-    { icon: <Plane size={20} />, label: 'Chuyến đi', to: '/trips' },
+    /*{ icon: <Plane size={20} />, label: 'Chuyến đi', to: '/trips' },*/
     { icon: <Star size={20} />, label: 'Đánh giá', to: '/reviews' },
-    { icon: <Heart size={20} />, label: 'Sở thích', to: '/interests' },
+    { icon: <Heart size={20} />, label: 'Danh mục sở thích', to: '/interests' },
     { icon: <Wallet size={20} />, label: 'Loại chi phí', to: '/expense-categories' },
-    { icon: <Tags size={20} />, label: 'Loại điểm đến', to: '/destination-types' },
+    { icon: <Tags size={20} />, label: 'Danh mục điểm đến', to: '/destination-types' },
     { icon: <ListTree size={20} />, label: 'Loại địa điểm', to: '/poi-types' },
   ];
 
